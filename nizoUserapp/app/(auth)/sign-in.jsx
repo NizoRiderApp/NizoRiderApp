37427a211705ab668eb1ae2a20ce82cd1a915961
McMillan <codecodexboot@gmail.com>
2024-07-31 22:09:01 +0200

    sign in and up pages added

--- nizoUserapp/app/(auth)/sign-in.jsx
+++ nizoUserapp/app/(auth)/sign-in.jsx
@@ -1,9 +1,43 @@
-import { View, Text, ScrollView, Image } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image, Button } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router"
 
 const SignIn = () => {
+
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [form, setForm] = useState({
+    fullName: "",
+    phoneNumber: "",
+    email: "",    
+    password: "",
+  });
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+
+    setSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLogged(true);
+
+      Alert.alert("Success", "User signed in successfully");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -16,6 +50,64 @@ const SignIn = () => {
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Nizo
           </Text>
+
+          {/* <FormField
+            title="Full Name"
+            value={form.fullName}
+            handleChangeText={(e) => setForm({ ...form, fullName: e })}
+            otherStyles="mt-7"
+            keyboardType="default"
+          />
+
+          <FormField
+            title="Phone Number"
+            value={form.phoneNumber}
+            placeholder= "(+27)"
+            handleChangeText={(e) => setForm({ ...form, phoneNumber: e})}
+            otherStyles="mt-7"
+            keyboardType="phone-pad"
+          /> */}
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e})}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
+
+          {/* <View style={styles.fileContainer}>
+            <Button title="Upload File" onPress={pickFile} />
+            {file && <Text>{file.name}</Text>}
+          </View>
+          <Button title="Submit" onPress={handleSubmit} /> */}
+
         </View>
       </ScrollView>
     </SafeAreaView>
